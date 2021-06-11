459cbcdaaf53b7c0e0e130a7f897209d14edbc57
toumplaris <85252253+toumplaris@users.noreply.github.com>
2021-06-11 22:45:35 +0300

    Add files via upload

---
+++ katigories.php
@@ -0,0 +1,55 @@
+﻿<?php
+include("header.php");
+echo "<div id=main>";
+
+  
+  // σε περίπτωση που έχει οριστεί με GET (sto URL) η κατηγορία τότε κάνει διαγραφή
+if (isset($_GET['delkatigoria']))
+{
+// εκτελώντας το παρακάτω ερώτημα
+   $sql="delete from katigories where katigoria='$_GET[delkatigoria]'";
+   mysql_query($sql);
+
+}
+
+
+// αν εχει πατηθεί το κουμπί προσθήκη
+if (isset($_POST['prosthiki']))
+{
+
+//εκτελεί προσθήκη κατηγορίας με το παρακάτω ερώτημα
+   $sql="insert into katigories(katigoria) values ('$_POST[katigoria]')";
+   mysql_query($sql);
+}
+
+// στείνω ένα πίνακα
+echo "<h2>Κατηγορίες</h2>";
+echo "<table >";
+
+// επιλέγω όλες τις κατηγορίες
+$sql="select * from katigories order by katigoria";
+$res=mysql_query($sql);
+
+// βάζω τους τίτλους στον πίνακα
+ echo "<tr><th></th><th>Ονομα Κατηγορίας</th></tr>";
+ 
+ // η φόρμα για την καταχώρηση κατηγορίας
+  echo "<tr><td></td><td><form action='katigories.php' method=post><input type=text name=katigoria><input type=submit name=prosthiki value='Προσθήκη'></form></td></tr>";
+
+  
+  // για κάθε εγγραφή (while) παίρνω την τιμή της κατηγορίας
+  while ($row=mysql_fetch_array($res))
+{
+
+// εμφανίζει την τιμή και τις επιλογές
+  echo "<tr><td><a href='katigories.php?delkatigoria=$row[katigoria]'>Διαγραφή</a></td><td>$row[katigoria]</td></tr>";
+ 
+
+}
+
+
+
+echo "</table>";
+  echo "</div>";
+include("footer.php");
+?>
---
+++ syndesi.php
@@ -0,0 +1,30 @@
+﻿<?php
+include("header.php");
+?>
+   <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+<div id=main>
+
+   
+   
+
+<h2>Σύνδεση Χρήστη</h2>
+Συμπληρώστε παρακάτω:
+<form action='index.php' method=post>
+<table>
+<tr><td>email</td><td><input type=email required name=email size=20></td></tr>
+<tr><td>Password</td><td><input type=password name=password required size=20></td></tr>
+<tr><td colspan=2><input type=submit name=syndesi value='Σύνδεση'></td></tr>
+</table>
+<br><br>
+</form>
+
+
+
+</div>
+<br><br><br><br>
+
+<?php
+
+include ("footer.php");
+
+?>
